4e86073b5c754d1272cdcb94cd216aace720f92d
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2026-09-10 13:39:44 +0530

    Migrate from commonJS to ESM

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,23 +1,23 @@
-const express = require('express');
-const router = express.Router();
+import express, {type Request, type Response, type Router} from 'express';
+const router: Router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/login', (req: Request, res: Response) => {
     res.send("Login route");
 });
-router.post('/logout', (req, res) => {
+router.post('/logout', (req: Request, res: Response)  => {
     res.send("Logout route");
 });
-router.post('/signup', (req, res) => {
+router.post('/signup', (req: Request, res: Response)  => {
     res.send("Create account route");
 });
-router.post('/forgot-password', (req, res) => {
+router.post('/forgot-password', (req: Request, res: Response)  => {
     res.send("Send mail/OTP. Forgot password route");
 });
-router.post('/reset-password', (req, res) => {
+router.post('/reset-password', (req: Request, res: Response)  => {
     res.send("reset password route");
 });
-router.get('/me', (req, res) => {
+router.get('/me', (req: Request, res: Response)  => {
     res.send("Get Current User Profile route");
 });
 
-module.exports = router;
+export default router;
