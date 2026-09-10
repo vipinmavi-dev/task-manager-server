201c0b29e500956e75ee11f678c6e117fd3754e9
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2026-09-10 15:04:15 +0530

    mysql db configuration done

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,15 @@
 import express, {type Request, type Response, type Router} from 'express';
 const router: Router = express.Router();
+import mysqlPool from '../configs/mysql.config.js';
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', async (req: Request, res: Response)  => {
     res.send("Login route");
 });
 router.post('/logout', (req: Request, res: Response)  => {
     res.send("Logout route");
 });
 router.post('/signup', (req: Request, res: Response)  => {
+    
     res.send("Create account route");
 });
 router.post('/forgot-password', (req: Request, res: Response)  => {
