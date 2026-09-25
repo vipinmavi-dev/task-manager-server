9ea4ca3f8d53a048bf60a46b7ab73eab5801adfe
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2026-09-25 12:07:39 +0530

    cookie config changed

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -37,7 +37,7 @@ router.post('/login', async (req: Request, res: Response) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: true,
-        sameSite: 'lax',
+        sameSite: 'none',
     });
 
     res.status(200).json({
